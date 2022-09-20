705a519a28795927e4a241618faac06c62a55e8d
Vanessa Motta <89320692+vanessafmotta@users.noreply.github.com>
2022-09-20 19:46:10 -0300

    Executing Commands

--- src/App.js
+++ src/App.js
@@ -3,12 +3,50 @@ import { createEditor } from 'slate'
 import { Slate, Editable, withReact } from 'slate-react'
 import { Editor, Transforms } from 'slate'
 
+const CustomEditor = {
+  isBoldMarkActive(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: n => n.bold === true,
+      universal: true,
+    })
+
+    return !!match
+  },
+
+  isCodeBlockActive(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: n => n.type === 'code',
+    })
+
+    return !!match
+  },
+
+  toggleBoldMark(editor) {
+    const isActive = CustomEditor.isBoldMarkActive(editor)
+    Transforms.setNodes(
+      editor,
+      { bold: isActive ? null : true },
+      { match: n => Text.isText(n), split: true }
+    )
+  },
+
+  toggleCodeBlock(editor) {
+    const isActive = CustomEditor.isCodeBlockActive(editor)
+    Transforms.setNodes(
+      editor,
+      { type: isActive ? null : 'code' },
+      { match: n => Editor.isBlock(editor, n) }
+    )
+  },
+}
+
 const initialValue = [
   {
     type: 'paragraph',
     children: [{ text: 'A line of text in a paragraph.' }],
   },
 ]
+
 
 const App = () => {
   const [editor] = useState(() => withReact(createEditor()))
@@ -22,25 +60,56 @@ const App = () => {
     }
   }, [])
 
+  const renderLeaf = useCallback(props => {
+    return <Leaf {...props} />
+  }, [])
+
   return (
     <Slate editor={editor} value={initialValue}>
+      <div>
+        <button
+          onMouseDown={event => {
+            event.preventDefault()
+            CustomEditor.toggleBoldMark(editor)
+          }}
+        >
+          Bold
+        </button>
+        <button
+          onMouseDown={event => {
+            event.preventDefault()
+            CustomEditor.toggleCodeBlock(editor)
+          }}
+        >
+          Code Block
+        </button>
+      </div>
       <Editable
         renderElement={renderElement}
+        renderLeaf={renderLeaf}
         onKeyDown={event => {
-          if (event.key === '`' && event.ctrlKey) {
-            event.preventDefault()
-            Transforms.setNodes(
-              editor,
-              { type: 'code' },
-              { match: n => Editor.isBlock(editor, n) }
-            )
+          if (!event.ctrlKey) {
+            return
+          }
+
+          switch (event.key) {
+            case '`': {
+              event.preventDefault()
+              CustomEditor.toggleCodeBlock(editor)
+              break
+            }
+
+            case 'b': {
+              event.preventDefault()
+              CustomEditor.toggleBoldMark(editor)
+              break
+            }
           }
         }}
       />
     </Slate>
   )
 }
-
 const CodeElement = props => {
   return (
     <pre {...props.attributes}>
@@ -52,4 +121,18 @@ const CodeElement = props => {
 const DefaultElement = props => {
   return <p {...props.attributes}>{props.children}</p>
 }
+
+const Leaf = props => {
+  return (
+    <span
+      {...props.attributes}
+      style={{ fontWeight: props.leaf.bold ? 'bold' : 'normal' }}
+    >
+      {props.children}
+    </span>
+  )
+}
+
+
+
 export default App;
